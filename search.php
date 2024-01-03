a6703b5af97cca70c6e8b36c228ab9d35389b292
deepak <deepakbaradwaj933@gmail.com>
2024-01-03 13:54:26 +0530

    footer change

--- search.php
+++ search.php
@@ -202,7 +202,51 @@ if (mysqli_num_rows($result) > 0) {
 
 
     </div>
-    <?php include './footer2.php' ?>
+    <footer>
+        <div class="new-footer-row">
+          <div class="new-footer-col">
+            <div class="new-footer-logo">
+            <img src="<?= get_url() ?>dashboard/assets/images/POOJA.webp.png" width="100%" height="100%" class="footer-logo-img" alt="">
+            </div>
+          </div>  
+          <div class="new-footer-col">
+            <strong>LINKS</strong>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact US</a></li>
+            </ul>
+          </div>  
+          <div class="new-footer-col">
+            <strong>HELP / INFO</strong>
+            <ul>
+                <li><a href="">Teams And Conditions</a></li>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Help and Faqs</a></li>
+            </ul>
+          </div>  
+          <div class="new-footer-col">
+            <strong>USEFUL LINKS</strong>
+            <ul>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">Post Your Ad</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+          </div>  
+        </div>
+        <div class="new-footer-row-2">
+            <p>The purpose of this website is to provide information between advertisers and entertainment seekers. We had never established any linkage with prositution, brokerage, pimp, escort service, brothels, etc. We are not accountable for any liability as we host third-party content, images, and phone numbers. We are not responsible for any transaction that took place between the model, advertiser, and client. Hereby, we request our visitors not to make any advance payments. We are just an online platform to advertise content and details on our web space. By visiting our website, you confirm the age of 18 and above.</p>
+        </div>
+        <div class="new-footer-row2">
+            <p>&copy; Copyrights 2023. All Rights Reserved</p>
+            <ul>
+                <li><i class="ri-facebook-circle-line"></i></li>
+                <li><i class="ri-instagram-line"></i></li>
+                <li><i class="ri-twitter-x-line"></i></li>
+                <li><i class="ri-linkedin-box-line"></i></li>
+            </ul>
+        </div>
+    </footer>
 
     <script>
         document.getElementById('serach-city-input').addEventListener('keyup', (e) => {
