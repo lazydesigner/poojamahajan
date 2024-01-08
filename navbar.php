e7c30656d8d1cd6191e447a88be942eb185bbed7
deepak <deepakbaradwaj933@gmail.com>
2024-01-08 16:03:50 +0530

    a

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <form action="<?= get_url() ?>search">
                 <div class="form-group">
-                    <select name="category" id="category" aria-label="category">
+                    <select name="category" id="category">
                         <option value="call-girls">Call Girls</option>
                         <option value="escorts">Escorts</option>
                     </select>
                     <!-- Search -->
-                    <div class="search-city" aria-label="search-city">
+                    <div class="search-city">
                             <input type="text" autocomplete="off" name="city" class="serach-city-input" style="text-transform: capitalize;" value="<?php if(isset($city)){echo $city;} ?>" id="serach-city-input" placeholder="Select City">
                             <div class="list-of-cities" id="list-of-cities">
                                 <ul style="border: 0;" id="list-of-cities-id">
@@ -14,10 +14,10 @@
                             </div>
                     </div>
                     <!-- Search -->
-                    <select name="locality" id="area-locality" aria-label="locality">
+                    <select name="locality" id="area-locality">
                         <option value="">All Location</option>
                     </select>
-                    <select name="gender" id="gender" aria-label="gender">
+                    <select name="gender" id="gender">
                         <option value="">All Gender</option>
                         <option value="female">Female</option>
                         <!-- <option value="male">Male</option>
@@ -26,15 +26,15 @@
                         <option value="gay-escort">Gay Escorts</option> -->
                     </select>
                 </div>
-                <div class="form-group" >
-                    <select name="age" id="age-group"  aria-label="age-group">
+                <div class="form-group">
+                    <select name="age" id="age-group">
                         <option value="">All Age Groups</option>
                         <option value="18-22">18 - 22 years</option>
                         <option value="23-26">23 - 26 years</option>
                         <option value="27-30">27 - 30 years</option>
                         <option value="35">35+ years</option>
                     </select>
-                    <select name="short" id="short"  aria-label="short-by">
+                    <select name="short" id="short">
                         <option value="">Short By</option>
                         <option value="new-first">New First</option>
                         <option value="old-first">Old First</option>
