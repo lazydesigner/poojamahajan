4b82f1a2032fdde869abe57539b9f08b4e637418
deepak <deepakbaradwaj933@gmail.com>
2024-04-17 14:33:59 +0530

    a

--- home.php
+++ home.php
@@ -769,12 +769,12 @@
                                                                                                     $res = mysqli_query($con, $sql);
                                                                                                     $row = mysqli_fetch_array($res);
                                                                                                     echo $row['a'] ?>)</a></li>
-                        <!-- <li><a href="<?= get_url() ?>call-girls/kalyani-nagar/">Kalyani Nagar(<?php $sql = "SELECT COUNT(*) as a FROM profiles WHERE catigory = 'call-girls' && state = 'kalyani-nagar';
+                        <li><a href="<?= get_url() ?>call-girls/jabalpur/">Jabalpur(<?php $sql = "SELECT COUNT(*) as a FROM profiles WHERE catigory = 'call-girls' && state = 'jabalpur';
 ";
                                                                                                 $res = mysqli_query($con, $sql);
                                                                                                 $row = mysqli_fetch_array($res);
                                                                                                 echo $row['a'] ?>)</a></li>
-                        --<li><a href="<?= get_url() ?>call-girls/dhanori/">Dhanori(<?php $sql = "SELECT COUNT(*) as a FROM profiles WHERE catigory = 'call-girls' && state = 'dhanori';
+                        <!--<li><a href="<?= get_url() ?>call-girls/dhanori/">Dhanori(<?php $sql = "SELECT COUNT(*) as a FROM profiles WHERE catigory = 'call-girls' && state = 'dhanori';
 ";
                                                                                     $res = mysqli_query($con, $sql);
                                                                                     $row = mysqli_fetch_array($res);
